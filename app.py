4f376940a0eee9ee61f853b9ffee097805cf8f9b
Charles Custer <charles-custer@uiowa.edu>
2023-04-04 21:33:43 -0500

    Flash Message

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_random_string"
 
 friends_dict = [
     {"name": "Test", "flavor": "swirl", "read": "yes", "activities": "reading"}
@@ -60,6 +61,11 @@ def add():
             friend_dict
         )  # append this dictionary entry to the larger friends dictionary
         print(friends_dict)
+
+        flash(
+            "The friend ;" + fname + " has been added to the database.",
+            "success",
+        )
         return redirect(url_for("index"))
     else:
         return redirect(url_for("index"))
